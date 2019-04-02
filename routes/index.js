55ab8378d0abdf3f8e15f61583e7f6a498d6fca7
Tyler Mikev <atmikev@gmail.com>
2019-04-01 21:32:45 -0500

    Code cleanup

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,6 @@ const express = require('express');
 const router = express.Router();
 const axios = require('axios');
 
-const twitterURL = "https://takehome.io/twitter"
-const facebookURL = "https://takehome.io/facebook"
-const instagramURL = "https://takehome.io/instagram"
-
 /* 
 Get all the social media info available
 Params:
@@ -13,37 +9,34 @@ Params:
 @param pollInterval - How often to check social media sites (in seconds)
 */
 router.get('/', async function(req, res, next) {
-    const params = req.params
-
+    
+    const endpoints = [
+      {
+        name: "instagram",
+        url: "https://takehome.io/instagram"
+      },
+      {
+        name: "facebook",
+        url: "https://takehome.io/facebook"
+      },
+      {
+        name: "twitter",
+        url: "https://takehome.io/twitter"
+      }
+    ];
+    
     let data = {};
-    
-    try {
-      const intagramResponse = await axios.get(instagramURL)
-      data.instagram = intagramResponse.data;
-      console.log(intagramResponse.data);
-    } catch (e) {
-      data.instagram = [];
-      console.log(e);
-    }
-    
-    try {
-      const facebookResponse = await axios.get(facebookURL)
-      data.facebook = facebookResponse.data;
-      console.log(facebookResponse.data);
-    } catch (e) {
-      data.facebook = [];
-      console.log(e);
-    }
 
-    try{
-      const twitterResponse = await axios.get(twitterURL)
-      data.twitter = twitterResponse.data;
-      console.log(twitterResponse.data);
-    } catch (e) {
-      data.twitter = [];
-      console.log(e);
+    for (endpoint of endpoints) {
+      try {
+        const response = await axios.get(endpoint.url);
+        console.log(response.data);
+        data[`${endpoint.name}`] = response.data;
+      }catch(e){
+        console.log(e);
+      }
     }
-
+  
     res.json(data);      
   
 });
